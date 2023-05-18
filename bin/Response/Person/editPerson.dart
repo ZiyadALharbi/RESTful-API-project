f06478793ff2400e4b6ab246e82fa85ca86c4781
ZiyadALharbi <ziyad.swengineer@gmail.com>
2023-05-18 14:26:42 +0300

    exam-1

--- bin/Response/Person/editPerson.dart
+++ bin/Response/Person/editPerson.dart
@@ -39,37 +39,3 @@ editPerson(Request req) async {
   }
 }
 
-
-
-// editPerson(Request req) async {
-//   try {
-//     File file = File("Person.txt");
-//     final body = json.decode(await req.readAsString());
-//     final lines = await file.readAsLines();
-
-//     final persons =
-//         lines.map(((line) => Person.fromJson(json.decode(line)))).toList();
-//     print("------here i am");
-//     final person = persons.firstWhere((person) => person.id == body["id"]);
-//     print(person);
-
-//     if (person != null) {
-//       person.name = body["name"];
-//       person.age = body["age"];
-//       person.address = body["address"];
-//     } else {
-//       return Response.notFound("Person not Found");
-//     }
-
-//     await file.writeAsString(
-//       "${json.encode(person.toMap())}\n",
-//       mode: FileMode.append,
-//     );
-
-//     return Response.ok("Person data is updated successfully!");
-//   } catch (error) {
-//     print("error: $error");
-
-//     return Response.notFound("Person not Found");
-//   }
-// }
